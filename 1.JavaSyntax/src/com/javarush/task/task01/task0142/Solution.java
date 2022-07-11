a4bbc5e504724e9b4dbe60db2a789086d7dfeb1e
Andrey <andrey.stolyarov1337@gmail.com>
2022-07-11 22:43:04 +0300

    add solution from JavaSyntax task 0142

--- 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task01.task0142;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println("Амиго очень умный");
     }
 }
