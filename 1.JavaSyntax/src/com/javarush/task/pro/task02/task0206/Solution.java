6af59bf0987f4168d1e3b6ea18904c420765237b
Andrey <andrey.stolyarov1337@gmail.com>
2022-07-12 23:51:28 +0300

    add solution from JavaSyntax task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "word";
+        String phrase = "phrase";
+        String line = "line";
+        String text = "text";
+
+    }
+}
