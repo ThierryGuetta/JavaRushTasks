a7ba1b699a069629b03e35ebbad79b5112c7b04f
Andrey <andrey.stolyarov1337@gmail.com>
2022-07-12 23:18:13 +0300

    add solution from JavaSyntax task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 5, numericValue = 6, size = 7, number = 8;
+    }
+}
