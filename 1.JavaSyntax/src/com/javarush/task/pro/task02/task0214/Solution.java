4d8ebbd5f991e209191ae1a41ee358d2ab5c54f5
Andrey <andrey.stolyarov1337@gmail.com>
2022-07-13 20:01:43 +0300

    add solution from JavaSyntax task 0214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        String c = scanner.nextLine();
+        System.out.println(c);
+        System.out.println(b.toUpperCase());
+        System.out.println(a.toLowerCase());
+
+    }
+}
