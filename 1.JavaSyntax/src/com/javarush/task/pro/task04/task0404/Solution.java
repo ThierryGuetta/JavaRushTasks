46bce1967e51e5f1de3d4231a2c82b2324991ae4
Andrey <andrey.stolyarov1337@gmail.com>
2022-07-14 18:19:36 +0300

    add solution from JavaSyntax task 0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            int x = 0;
+            while (x < 10) {
+                System.out.print("Q");
+                x++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
