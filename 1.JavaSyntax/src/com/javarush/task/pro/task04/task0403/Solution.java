1b236521284bd1298c6880f94efbddeb8197b6f1
Andrey <andrey.stolyarov1337@gmail.com>
2022-07-15 22:25:35 +0300

    add solution from JavaSyntax task 0403

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -9,5 +9,18 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true){
+            if(scanner.hasNextInt()){
+                int num = scanner.nextInt();
+                sum = sum + num;
+            } else if (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                if(string.equals("ENTER")){
+                    break;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
