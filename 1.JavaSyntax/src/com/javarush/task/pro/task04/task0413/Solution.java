15e7d3bef026bf571d6b9fea9241087a934e2c14
ThierryGuetta <andrey.stolyarov1337@gmail.com>
2022-07-19 16:24:26 +0300

    add solution from JavaSyntax task 0413

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -6,7 +6,12 @@ package com.javarush.task.pro.task04.task0413;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        for (int i = 0; i < 10; i++) {
+            int starCount = 1 + i;
+            for (int j = 0; j < starCount; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
