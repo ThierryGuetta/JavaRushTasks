6b8e944b041c0a30eea4d731233ee95c29c58275
ThierryGuetta <andrey.stolyarov1337@gmail.com>
2022-07-20 22:02:11 +0300

    add solution from JavaSyntax task 0415

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -8,7 +8,11 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        double pi = 3.14;
+        double s = pi * radius * radius;
+        int S = (int) (s);
+        System.out.println(S);
     }
 }
