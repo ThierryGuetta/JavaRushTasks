b0afc5eea7326221222ac35de5df9bd14e9b5e9e
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-01 20:37:04 +0300

    add solution from JavaSyntax My Training 004

---
+++ 1.JavaSyntax/src/ForTriangle002.java
@@ -0,0 +1,11 @@
+public class ForTriangle002 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 20; i++) {
+            int stringCount = 40 - i;
+            for (int j = 40; j > stringCount; j--) {
+                System.out.print("b");
+            }
+            System.out.println();
+        }
+    }
+}
