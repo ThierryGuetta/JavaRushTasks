7f0b9c56ccd9bde92c462d49e96afb69697fc4f3
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-03 20:04:54 +0300

    add solution from JavaSyntax ArrayReversOrderTest

---
+++ 1.JavaSyntax/src/ArrayReverseOrder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ArrayReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[scanner.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[array.length - 1 - i]);
+        }
+    }
+}
