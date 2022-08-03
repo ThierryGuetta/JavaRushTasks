1f74a43e6df3f160559dfee3aa90a1a856758058
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-03 20:18:55 +0300

    add solution from JavaSyntax task 0507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task05.task0507;
+
+import java.util.Scanner;
+
+/* 
+Максимальное из N чисел
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        array = new int[scanner.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
