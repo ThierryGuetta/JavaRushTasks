e176f936f7a70633200a9db9457da9fe5317a480
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-04 16:36:41 +0300

    add solution from JavaSyntax task 0509

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task05.task0509;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+
+    public static int[][] MULTIPLICATION_TABLE;
+
+    public static void main(String[] args) {
+        MULTIPLICATION_TABLE = new int[10][10];
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                MULTIPLICATION_TABLE[i - 1][j - 1] = i * j;
+                System.out.print(MULTIPLICATION_TABLE[i - 1][j - 1] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
