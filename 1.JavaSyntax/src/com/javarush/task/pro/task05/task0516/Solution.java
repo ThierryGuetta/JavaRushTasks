f41188813518516d6ccdcb247c42521361fb40dd
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-08 19:46:52 +0300

    add solution from JavaSyntax task 0518

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task05.task0516;
+
+import java.util.Arrays;
+
+/* 
+Заполняем массив
+*/
+
+public class Solution {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(array));
+    }
+}
