f3927a0f9aa11e4d45ad479520a75f8bf3b5df1b
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-09 17:11:13 +0300

    add solution from JavaSyntax DoubleTriangle

--- 1.JavaSyntax/src/MyTests/DoubleTriangle.java
+++ 1.JavaSyntax/src/MyTests/DoubleTriangle.java
@@ -2,16 +2,10 @@ package MyTests;
 
 public class DoubleTriangle {
     public static void main(String[] args) {
-        for (int i = 0; i < 22; i++) {
-            if (i > 10) {
-                continue;
-            }
-            for (int j = 0; j < 22 - i; j++) {
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 20; j++) {
                 System.out.print("*");
-                if (j == 10 - i) System.out.print(" ");
-                else if (j > 10) {
-                    break;
-                }
+                if (j == 18 - i) System.out.print(" ");
             }
             System.out.println();
         }
