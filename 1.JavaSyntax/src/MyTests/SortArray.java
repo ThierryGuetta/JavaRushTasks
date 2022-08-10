2cb44e52c285882eb53d15c3ed4fd3f7fc785132
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-10 14:03:43 +0300

    add solution from JavaSyntax SortArray

--- 1.JavaSyntax/src/MyTests/SortArray.java
+++ 1.JavaSyntax/src/MyTests/SortArray.java
@@ -1,17 +1,20 @@
 package MyTests;
+import java.util.Arrays;
 
 public class SortArray {
     public static void main(String[] args) {
-        int[] array = {-166, -255, 3, 4, 1, 2, 8, 2, 4, 3, 6, 98, 4};
-        for (int i = array.length - 1; i > 0; i--) {
+
+        int[] numbers = {167, -2, 16, 99, 26, 92, 43, -234, 35, 80};
+
+        for (int i = numbers.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
+                if (numbers[j] > numbers[j + 1]) {
+                    int tmp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = tmp;
                 }
             }
-            System.out.println(array[i]);
         }
+        System.out.println(Arrays.toString(numbers));
     }
 }
