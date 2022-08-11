893aa7aa2deb6d21be772d6f5c146b75cda1118a
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-11 13:24:42 +0300

    add solution from JavaSyntax task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int age1) {
+
+    }
+
+    public static void universalMethod(char age2, String name2) {
+
+    }
+
+    public static void universalMethod(long age3, String name3) {
+
+    }
+
+    public static void universalMethod(int age4, char name4) {
+
+    }
+
+    public static void universalMethod(String age5, String name5) {
+
+    }
+
+    public static void universalMethod(int age6, int name6) {
+
+    }
+
+    public static void universalMethod(double age7, String name7) {
+
+    }
+
+    public static void universalMethod(float age8, String name8) {
+
+    }
+
+    public static void universalMethod(float age9, float name9) {
+
+    }
+
+    public static void universalMethod(int age10, String name10) {
+
+    }
+}
