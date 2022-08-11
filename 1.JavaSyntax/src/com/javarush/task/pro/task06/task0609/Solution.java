42796fa847b056773f0bc50481fa2da9bc3ccd38
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-11 13:51:27 +0300

    add solution from JavaSyntax task 0609

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0609/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0609/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
         return a * a * a;
     }
 
-    public static long ninthDegree(long b){
+    public static long ninthDegree(long b) {
         return cube(cube(b));
     }
 }
