0ae8afe4f1c0a94f2b5cc40577c4d4db6ae6b131
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-12 16:27:29 +0300

    add solution from JavaSyntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int i = 4;
+    double a = 3.14;
+    float b = 3.14f;
+    long c = 5l;
+    boolean d = true;
+    short e = 1;
+    byte t = 9;
+    char y = 65000;
+}
+
