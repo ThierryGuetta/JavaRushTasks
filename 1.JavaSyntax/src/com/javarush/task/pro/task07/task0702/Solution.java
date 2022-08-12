b2c8ea76b047b1c9ac9a84112ff8b59fb8ffbcf4
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-12 16:31:53 +0300

    add solution from JavaSyntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int q = 2147483647;
+    short w = 32767;
+    byte e = 127;
+    long r = Long.MAX_VALUE;
+}
