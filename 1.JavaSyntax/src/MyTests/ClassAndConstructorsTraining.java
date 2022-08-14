e2d5f3a4ba63608b27a01f5740bc38d37f44b103
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-14 16:26:40 +0300

    add solution from JavaSyntax ClassAndConstructorsTraining

---
+++ 1.JavaSyntax/src/MyTests/ClassAndConstructorsTraining.java
@@ -0,0 +1,36 @@
+package MyTests;
+
+public class ClassAndConstructorsTraining {
+    public static void main(String[] args) {
+        Person student = new Person();
+        student.name = "Andrey";
+        student.age = 24;
+        student.hobby = "fishing";
+        student.work = "manager";
+        student.experience = 6;
+        student.personBio();
+        student.personBio(student.hobby);
+        student.personBio(student.work, student.experience);
+
+    }
+}
+class Person {
+    String name;
+    int age;
+    String hobby;
+    String work;
+    int experience;
+
+    void personBio() {
+        System.out.println("Student bio: " + name + " " + age + " y.o.");
+    }
+
+    void personBio(String hobby) {
+        System.out.println(name + "'s" + " hobby is " + hobby);
+    }
+
+    void personBio(String work, int experience) {
+        System.out.println(name + "'s" + " work is " + work);
+        System.out.println(name + "'s" + " experience is " + experience + " years");
+    }
+}
