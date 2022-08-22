e86b9b100e284ac4f287cda75d30f3279a4d8324
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-22 18:39:11 +0300

    add solution from JavaSyntax task 0909

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
@@ -6,6 +6,7 @@ package com.javarush.task.pro.task09.task0909;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
     }
 }
