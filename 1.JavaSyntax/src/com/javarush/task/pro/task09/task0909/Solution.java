07f2bc9758375e9439510bfee121fc25cbf08e00
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-22 18:36:03 +0300

    add solution from JavaSyntax task 0909

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task09.task0909;
+
+/* 
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
