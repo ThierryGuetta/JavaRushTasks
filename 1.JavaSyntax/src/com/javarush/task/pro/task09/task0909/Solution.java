aeca1d0e76407b86b9f26df4f379c2d1ddf3fa96
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-22 18:37:24 +0300

    add solution from JavaSyntax task 0909

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.pro.task09.task0909;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println();
     }
 }
