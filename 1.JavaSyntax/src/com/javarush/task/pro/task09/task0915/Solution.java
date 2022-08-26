20ce5dd8a7be82216847b89f4cf841f568cab5d2
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-26 14:42:54 +0300

    add solution from JavaSyntax task 0915

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0915/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task09.task0915;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String packagePath = "java.util.stream";
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer stringTokenizer = new StringTokenizer(query, delimiter);
+        String[] array = new String[stringTokenizer.countTokens()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = stringTokenizer.nextToken();
+        }
+        return array;
+    }
+}
