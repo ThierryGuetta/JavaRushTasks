f8100a3c57dcddb091e05e3a38a49e313021bd64
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-26 16:03:09 +0300

    add solution from JavaSyntax task 0917

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0917/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task09.task0917;
+
+/* 
+String pool
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String first = new String("JavaRush");
+        String second = new String("JavaRush");
+        String third = new String("javarush");
+        System.out.println(equal(first, second));
+        System.out.println(equal(second, third));
+    }
+
+    public static boolean equal(String first, String second) {
+        String a1 = first.intern();
+        String a2 = second.intern();
+        if (a1 == a2) return true;
+        else return false;
+    }
+}
