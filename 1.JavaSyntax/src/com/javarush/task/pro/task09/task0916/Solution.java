9112e3d1ef45a067030c1261dafab6c4c37d2db2
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-26 15:41:02 +0300

    add solution from JavaSyntax task 0916

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0916/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task09.task0916;
+
+/* 
+String.format()
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(format("Амиго", 5000));
+    }
+
+    public static String format(String name, int salary) {
+        return String.format("Меня зовут %s. Я буду зарабатывать $%d в месяц.", name, salary);
+    }
+}
