23b81cc8aa22f6bc6ba5042495f713a81b0d666c
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-03 18:46:28 +0300

    add solution from JavaSyntax task 1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer() {
+        Monitor monitor = new Monitor();
+        Keyboard keyboard = new Keyboard();
+        Mouse mouse = new Mouse();
+        SystemUnit systemUnit = new SystemUnit();
+    }
+}
