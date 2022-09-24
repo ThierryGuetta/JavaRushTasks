2ecc7e0176af8532066919fc5187f9389db5779e
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-24 22:38:35 +0300

    add solution from JavaSyntax task 1512

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1512/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task15.task1512;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/* 
+Задом наперед
+*/
+
+public class Solution {
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(scanner.nextLine());
+        printSomething(sb.reverse().toString());
+        String result = outputStream.toString();
+        System.out.println(result);
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
+    }
+}
