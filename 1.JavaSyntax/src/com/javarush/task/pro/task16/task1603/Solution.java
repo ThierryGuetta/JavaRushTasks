3be8a311118eace8e4d46a8599b378b9a4dbf5df
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-26 00:20:44 +0300

    add solution from JavaSyntax task 1603

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1603/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1603/Solution.java
@@ -12,6 +12,6 @@ public class Solution {
     public static void main(String[] args) {
         Date date = new Date("6 January 2010");
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(formatter.format(date));
+        System.out.println(formatter.format(date))
     }
 }
