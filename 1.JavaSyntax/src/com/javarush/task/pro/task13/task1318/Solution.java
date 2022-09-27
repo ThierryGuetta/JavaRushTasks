9677d4c472237908b58b6e89096c7a2a9115d87d
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-27 18:34:03 +0300

    add solution from JavaSyntax task 1318

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Solution.java
@@ -4,19 +4,16 @@ package com.javarush.task.pro.task13.task1318;
 Следующий месяц, пожалуйста
 */
 
-import java.util.Arrays;
-
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.DECEMBER));
         System.out.println(getNextMonth(Month.JULY));
     }
 
     public static Month getNextMonth(Month month) {
-        Month[] array = Month.values();
-        if (month.ordinal() == array.length - 1) return array[0];
-        else return array[month.ordinal() + 1];
+        Month[] months = Month.values();
+        if (month.ordinal() < 11) return months[month.ordinal() + 1];
+        else return months[0];
     }
 }
-
