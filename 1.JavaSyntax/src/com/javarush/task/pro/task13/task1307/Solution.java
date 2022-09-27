4570e5b081d3036755bb913e26e349b8791925b2
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-27 15:32:58 +0300

    add solution from JavaSyntax task 1307

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1307/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1307/Solution.java
@@ -22,10 +22,24 @@ public class Solution {
         Collections.rotate(list, distance);
     }
 
-
     public static void shuffle(ArrayList<Integer> list) {
-        Random rnd = new Random();
-        Collections.shuffle(list, rnd);
+        Collections.shuffle(list);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            integers.add((int) (Math.random() * 10));
+        }
+        System.out.println("original " + integers);
+        reverse(integers);
+        System.out.println("reverse " + integers);
+        sort(integers);
+        System.out.println("sort " + integers);
+        rotate(integers, 1);
+        System.out.println("rotate " + integers);
+        shuffle(integers);
+        System.out.println("shuffle " + integers);
+
     }
 }
-
