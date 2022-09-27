46fe38001e063adcc911b457108ee5b258cdd14a
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-27 15:35:16 +0300

    add solution from JavaSyntax task 1309

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1309/Solution.java
@@ -15,10 +15,10 @@ public class Solution {
     }
 
     public static void addStudents() {
-        grades.put("Ivan", 2.4);
-        grades.put("Oleg", 2.5);
-        grades.put("Sasha", 2.6);
-        grades.put("Andrey", 2.7);
-        grades.put("Olya", 2.8);
+        grades.put("Alex", 5.9);
+        grades.put("Andrey", 5.9);
+        grades.put("Misha", 5.9);
+        grades.put("Ivan", 5.9);
+        grades.put("Petr", 5.9);
     }
 }
