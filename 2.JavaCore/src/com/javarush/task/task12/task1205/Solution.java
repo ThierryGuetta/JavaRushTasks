b320b675bde37952d6a009c47f044575eec818d3
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-23 20:04:31 +0300

    add solution from JavaCore task 1205

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1205/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task12.task1205;
+
+/* 
+Определимся с животным
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Dog()));
+        System.out.println(getObjectType(new Whale()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        if (Cow.class.equals(o.getClass())) {
+            return "Корова";
+        } else if (Dog.class.equals(o.getClass())) {
+            return "Собака";
+        } else if (Whale.class.equals(o.getClass())) {
+            return "Кит";
+        } else return "Неизвестное животное";
+    }
+
+    public static class Cow {
+    }
+
+    public static class Dog {
+    }
+
+    public static class Whale {
+    }
+
+    public static class Pig {
+    }
+}
