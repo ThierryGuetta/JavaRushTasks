a11d5a7b70a0a1fe076f7761bd71d3936295be86
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-23 23:13:35 +0300

    add solution from JavaCore task 1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i) {
+    }
+
+    public static void print(int i, short f) {
+    }
+
+    public static void print(int i, double u) {
+    }
+
+    public static void print(int i, String c) {
+    }
+
+    public static void print(int i, int k) {
+    }
+
+}
