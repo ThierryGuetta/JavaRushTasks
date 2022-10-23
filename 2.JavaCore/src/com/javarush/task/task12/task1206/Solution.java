fc443920cd56c391a8a194c8d73fe3e6efcf41d7
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-23 20:05:56 +0300

    add solution from JavaCore task 1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int s) {
+    }
+
+    public static void print(String s) {
+    }
+}
