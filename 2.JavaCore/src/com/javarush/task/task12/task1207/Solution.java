1c69dbc5b45b90b8995c688fb18ca6b675eb2fe6
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-23 20:08:51 +0300

    add solution from JavaCore task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(10);
+        print(new Integer(10));
+    }
+
+    public static void print(int s) {
+    }
+
+    public static void print(Integer s) {
+    }
+}
