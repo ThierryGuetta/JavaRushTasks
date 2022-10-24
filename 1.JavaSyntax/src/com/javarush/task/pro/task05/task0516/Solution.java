f1ded1ab09217f33f1013bdf5b59ad688e8f102d
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-24 20:08:01 +0300

    add solution from JavaSyntax task 0506

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -8,11 +8,14 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static int[] array = new int[20];
+    public static int[] array = new int[9];
     public static int valueStart = 10;
     public static int valueEnd = 13;
 
     public static void main(String[] args) {
+        int firstPart = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
+        Arrays.fill(array, 0, firstPart, valueStart);
+        Arrays.fill(array, firstPart, array.length, valueEnd);
         System.out.println(Arrays.toString(array));
     }
 }
