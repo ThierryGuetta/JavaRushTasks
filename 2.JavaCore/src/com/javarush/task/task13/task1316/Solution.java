4668c92a69cdc8c705b0d3eb512ee3f0430f49e4
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-26 00:43:33 +0300

    add solution from JavaCore task 1316

--- 2.JavaCore/src/com/javarush/task/task13/task1316/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1316/Solution.java
@@ -18,19 +18,8 @@ public class Solution {
 
         final String NAME = "Submit";
 
-        public void onPress();
-
-        protected void onPress();
-
-        void onPress();
-
-        private void onPress();
-
-        protected String onPress(Object o);
-
         String onPress(Object o);
 
-        private String onPress(Object o);
 
     }
 }
