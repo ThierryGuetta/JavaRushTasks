2ab98f6a21932cc2851968d836b318385532703e
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-26 17:59:21 +0300

    add solution from JavaCore task 1318

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.task.task13.task1318;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -11,22 +10,21 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        FileInputStream fis = new FileInputStream(scanner.nextLine());
-        InputStreamReader isr = new InputStreamReader(fis);
-        BufferedReader br = new BufferedReader(isr);
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(sourceFileName);
 
-        while (fis.available() >= 0) {
-            String i = br.readLine();
-            if (i == null) {
-                break;
-            }
-            System.out.println(i);
+        Scanner scanner = new Scanner(fileInputStream);
+        StringBuilder builder = new StringBuilder();
+
+        while (scanner.hasNextLine()) {
+            builder.append(scanner.nextLine()).append("\n");
         }
+
+        System.out.print(builder.toString());
+
         scanner.close();
-        isr.close();
-        fis.close();
-        br.close();
+        reader.close();
     }
 }
