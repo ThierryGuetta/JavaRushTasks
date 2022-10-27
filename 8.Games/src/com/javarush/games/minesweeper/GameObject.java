00de89cebc98e9d5222441d155034408bebc29ab
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-28 01:45:34 +0300

    add solution from JR MinesweeperGame Task 05

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -1,10 +1,14 @@
 package com.javarush.games.minesweeper;
 
-public class GameObject {
-    public int x, y;
+import com.javarush.engine.cell.*;
 
-    public GameObject(int x, int y) {
+public class GameObject extends Game {
+    public int x, y;
+    public boolean isMine;
+
+    public GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
+        this.isMine = isMine;
     }
 }
--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -2,11 +2,10 @@ package com.javarush.games.minesweeper;
 
 import com.javarush.engine.cell.*;
 
-import java.util.Arrays;
-
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
     private final GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField;
 
     @Override
     public void initialize() {
@@ -15,9 +14,15 @@ public class MinesweeperGame extends Game {
     }
 
     private void createGame() {
+
         for (int i = 0; i < gameField.length; i++) {
             for (int j = 0; j < gameField.length; j++) {
-                gameField[i][j] = new GameObject(j, i);
+                int rnd = getRandomNumber(10);
+                boolean isMine = rnd < 1;
+                if (isMine) {
+                    countMinesOnField++;
+                }
+                gameField[i][j] = new GameObject(j, i, isMine);
                 setCellColor(i, j, Color.ORANGE);
             }
         }
