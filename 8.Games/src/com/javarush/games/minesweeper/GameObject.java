be498663de38e51fd5d5f03748fb7570815eb48d
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-28 00:43:13 +0300

    add solution from JR MinesweeperGame Task 03

---
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -0,0 +1,10 @@
+package com.javarush.games.minesweeper;
+
+public class GameObject {
+    public int x, y;
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
