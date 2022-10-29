b62a478ae2d0268a429f2092d5a0507e8193c0cd
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-29 06:16:58 +0300

    add solution from JavaCore task 1410

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Drink.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task14.task1410;
+
+abstract class Drink {
+    public void taste(){
+        System.out.println("Вкусно");
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task14.task1410;
+
+/* 
+Дегустация вин
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        getDeliciousDrink().taste();
+        System.out.println(getWine().getHolidayName());
+        System.out.println(getSparklingWine().getHolidayName());
+        System.out.println(getWine().getHolidayName());
+    }
+
+    public static Drink getDeliciousDrink() {
+        Drink wine = new Wine();
+        return wine;
+    }
+
+    public static Wine getWine() {
+        return new Wine();
+    }
+
+    public static Wine getSparklingWine() {
+        return new SparklingWine();
+    }
+}
