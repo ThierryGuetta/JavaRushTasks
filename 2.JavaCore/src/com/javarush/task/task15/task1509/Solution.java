ea3c937321acd2b980336b96078d66bece909612
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-30 21:48:21 +0300

    add solution from JavaCore task 1509

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1509/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1509;
+
+/* 
+Мужчина или женщина?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new Woman();
+
+        printName(man);
+        printName(woman);
+    }
+
+    public static void printName(Human man) {
+
+    }
+
+    public static class Human {
+
+    }
+
+    public static class Man extends Human{
+
+    }
+
+    public static class Woman extends Human{
+
+    }
+}
