ca701b1bb38eb3051d0a6b9e232c01a2eb5ae5ce
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-30 22:07:48 +0300

    add solution from JavaCore task 1507

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1507/Solution.java
@@ -0,0 +1,59 @@
+package com.javarush.task.task15.task1507;
+
+/* 
+ООП - Перегрузка
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        printMatrix(2, 3, "8");
+    }
+
+    public static void printMatrix(int m, int n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+    public static void printMatrix(int m, int n, Float value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Character value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Byte value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Integer value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Short value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Double value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Solution value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+    public static void printMatrix(int m, int n, Scanner value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+}
