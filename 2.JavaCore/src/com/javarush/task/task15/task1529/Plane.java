95228e7cbece6d689aaf2b6a343b1c1aed3a97b2
Andrey <andrey.stolyarov1337@gmail.com>
2022-11-01 00:27:32 +0300

    add solution from JavaCore task 1529

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly{
+    int passengers;
+
+    public Plane(int passengers) {
+        this.passengers = passengers;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task15.task1529;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Осваивание статического блока
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static {
+        reset();
+    }
+
+    public static CanFly result;
+
+    public static void reset() {
+        Scanner scanner = new Scanner(System.in);
+        String fly = scanner.nextLine();
+        if (fly.equals("plane")) result = new Plane(scanner.nextInt());
+        else if (fly.equals("helicopter")) result = new Helicopter();
+    }
+}
