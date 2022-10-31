e208de988ea1381c029d40942795184848079f5e
Andrey <andrey.stolyarov1337@gmail.com>
2022-11-01 01:35:13 +0300

    add solution from JavaCore task 1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution(int w) {
+    }
+
+    protected Solution(double e) {
+    }
+
+    Solution(long d) {
+    }
+
+    private Solution(String d) {
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution(int w) {
+        super(w);
+    }
+
+    protected SubSolution(double e) {
+        super(e);
+    }
+
+    SubSolution(long d) {
+        super(d);
+    }
+}
