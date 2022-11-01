51a1c0bdf0e8ef8e72bb6194b57f510d52df44e1
Andrey <andrey.stolyarov1337@gmail.com>
2022-11-02 02:00:43 +0300

    add solution from JavaCore task 1528

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1528/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task15.task1528;
+
+/* 
+ООП. Hryvnia — тоже деньги
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Hryvnia().getAmount());
+    }
+
+    public static abstract class Money {
+        abstract Money getMoney();
+
+        public Object getAmount() {
+            return getMoney().getAmount();
+        }
+    }
+
+
+    public static class Hryvnia extends Money {
+        private double amount = 123d;
+
+        @Override
+        public Object getAmount() {
+            return amount;
+        }
+
+        public Hryvnia getMoney() {
+            return this;
+        }
+    }
+}
