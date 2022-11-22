677461fab69f42c2307e51b6d68ad5a145d86b24
Andrey <andrey.stolyarov1337@gmail.com>
2022-11-22 15:16:30 +0300

    add solution from JavaCore task 1901

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1901/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task19.task1901;
+
+/* 
+TableAdapter
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //это пример вывода
+        ATable aTable = new ATable() {
+            @Override
+            public String getCurrentUserName() {
+                return "Amigo";
+            }
+
+            @Override
+            public String getTableName() {
+                return "DashboardTable";
+            }
+        };
+
+        BTable table = new TableAdapter(aTable);
+        System.out.println(table.getHeaderText());
+    }
+
+    public static class TableAdapter implements BTable{
+        private ATable aTable;
+        @Override
+        public String getHeaderText() {
+            return "[" + aTable.getCurrentUserName() + "]" + " : " + aTable.getTableName();
+        }
+        TableAdapter(ATable aTable){
+            this.aTable = aTable;
+        }
+    }
+
+    public interface ATable {
+        String getCurrentUserName();
+
+        String getTableName();
+    }
+
+    public interface BTable {
+        String getHeaderText();
+    }
+}
