c588fdc769d20b9dba7cf74282c25d6b0ec27b45
ThierryGuetta <andrey.stolyarov1337@gmail.com>
2023-01-02 18:46:33 +0300

    add solution from JavaCollections task 3102

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+/* 
+Находим все файлы
+*/
+
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        File file = new File(root);
+        List<String> paths = new ArrayList<>();
+        Queue<File> queue = new PriorityQueue<>();
+
+        Collections.addAll(queue, file.listFiles());
+
+        while (!queue.isEmpty()) {
+            File currentFIle = queue.remove();
+            if (currentFIle.isDirectory()) {
+                Collections.addAll(queue, currentFIle.listFiles());
+            } else paths.add(currentFIle.getAbsolutePath());
+        }
+        return paths;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
