4b1bc90040cbb9c93549b0ba60d00a4bd7e46b7f
ThierryGuetta <andrey.stolyarov1337@gmail.com>
2023-01-08 16:35:29 +0300

    add solution from Java Collections task 3109

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        File file = new File(fileName);
+        Properties properties = new Properties();
+        try {
+            if (file.getName().endsWith("xml")) properties.loadFromXML(new FileInputStream(file));
+            else properties.load(new FileReader(file));
+        } catch (Exception e) {
+            return new Properties();
+        }
+        return properties;
+    }
+}
